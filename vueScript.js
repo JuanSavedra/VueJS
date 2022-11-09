b2a21467f73ff6c96f7e7fd4c6180928d3b24698
Juan Savedra <juansavedra827@gmail.com>
2022-11-09 15:51:51 -0300

    v-bind

--- vueScript.js
+++ vueScript.js
@@ -1,21 +1,9 @@
 const vm = new Vue ({
     el: "#app",
     data: {
-        number1: 2,
-        number2: 2
+        site: "https://google.com",
+        color: "green",
+        target: "_blank"
     },
-    methods: {
-        sum: function sum() {
-            return this.number1 + this.number2;
-        },
-        subtraction: function() {
-            return this.number1 - this.number2;
-        },
-        multiplication: () => {
-            return 2 * 2; //Não pode recuperar os atributos do data por this
-        },
-        division() {
-            return this.number1 / this.number2;
-        }
-    }
+    methods: {}
 });
