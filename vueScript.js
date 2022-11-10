b41b32d3997f2869328191836af5d1beefd36345
Juan Savedra <juansavedra827@gmail.com>
2022-11-10 01:50:54 -0300

    v-html

--- vueScript.js
+++ vueScript.js
@@ -1,18 +1,7 @@
 const vm = new Vue ({
     el: "#app",
     data: {
-        color: "default", 
-        coordX: 0,
-        coordY: 0
+        htmlElements: "<p>Hello World</p><br><a href='https://google.com'>Google</a>"
     },
-    methods: {
-        changeColor: function(color) {
-            return this.color = color;
-        },
-        currentCoordenates: function(event) {
-            this.coordX = event.clientX;
-            this.coordY = event.clientY;
-            return event;
-        }
-    }
+    methods: {}
 });
